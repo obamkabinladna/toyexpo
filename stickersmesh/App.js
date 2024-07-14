482c649cdaf9a4c3cc88d0b59011c0e6427c92c4
gmoneycodes <greg@ruyoga.io>
2024-07-14 16:39:12 +0700

    move image component to components/ImageViewer

--- stickersmesh/App.js
+++ stickersmesh/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image} from 'react-native';
 
+import ImageViewer from './components/ImageViewer'
+
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style = {styles.imageContainer}>
-          <Image source={PlaceholderImage} style={styles.image}></Image>
+          <ImageViewer source={PlaceholderImage} style={styles.image}></ImageViewer>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -23,10 +25,5 @@ const styles = StyleSheet.create({
     imageContainer: {
         flex: 1,
         paddingTop: 58,
-    },
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18
     },
 });
